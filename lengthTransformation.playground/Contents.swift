05dbac5d362531d2e9561d040afb422a7118c095
waitnsee <yellow.cjh@gmail.com>
2017-10-16 17:10:55 +0900

    첫번째 단위변환

--- lengthTransformation.playground/Contents.swift
+++ lengthTransformation.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+/*
+ * 요구사항
+ github 저장소를 만들고 새로운 Playground를 생성해서 저장소에 연결한다.
+ 다음의 센티미터(cm) 단위 값을 미터(m) 단위로 변환해 결과 화면에 출력한다.
+ 예를 들어 "120cm"을 "1.2m"로 출력한다.
+ 미터(m) 단위 값을 센티미터(cm) 단위로 변환해 결과 화면에 출력한다.
+ "1.86m"를 "186cm"로 출력한다.
+ 
+ *힌트
+ 
+ 변환 결과를 print() 함수를 활용해 결과 화면에 출력한다.
+ Int 타입과 Float/Double 데이터 타입 변환하는 방법을 찾아본다.
+ 구글에서 "프로그래밍 길이 변환" 혹은 "프로그래밍 단위 변환" 같은 키워드로 검색하고 변환(계산) 방법을 찾아 구현한다.
+ */
+
+// 120cm
+
+var aLength: Double = 120
+
+print("\(aLength/100)m")
+
+var bLength: Double = 1.86
+
+print("\(Int(bLength*100))cm")
+
+
+
