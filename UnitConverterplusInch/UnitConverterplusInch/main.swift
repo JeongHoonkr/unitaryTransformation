5f89463637beb49e7d406b82a36599373f30a568
waitNseE <yellow.cjh@gmail.com>
2017-10-24 17:41:11 +0900

    enum및 struct활용하여 전체적으로 수정하여 구현

--- UnitConverterplusInch/UnitConverterplusInch/main.swift
+++ UnitConverterplusInch/UnitConverterplusInch/main.swift
@@ -1,81 +1,170 @@
 //
 //  main.swift
 //  UnitConverterplusInch
-//
 //  Created by Choi Jeong Hoon on 2017. 10. 22..
 //  Copyright © 2017년 JH Factory. All rights reserved.
-//
 
-import Foundation
 
-/*
- 사용자가 길이 값과 단위를 입력하고 변환할 단위까지도 입력하도록 확장한다.
- 센티미터를 인치로 바꾸는 함수와 인치를 센티미터로 바꾸는 함수를 추가로 구현한다.
- 사용자가 입력한 문자열에서 값 뒤에 붙어있는 단위와 그 이후에 변환할 단위를 붙이면 해당하는 변환 함수를 호출해서 변환하도록 구현한다.
- 예를 들어 "18cm inch"라고 입력하면 센티미터를 인치로 바꾸는 함수를 호출한다.
- "25.4inch m"라고 입력하면 인치를 센티미터로 바꾸는 함수를 호출하고, 다시 센티미터 단위를 미터로 바꾸는 함수를 호출한다.
- "0.5m inch"라고 입력하면 미터를 센티미터로 바꾸는 함수를 호출하고, 다시 센티미터 단위를 인치로 바꾸는 함수를 호출한다.
- "183cm"처럼 숫자 다음에 cm만 붙어있으면 센티미터 값을 미터로 변환하고 출력한다.
- "3.14m"처럼 숫자 다음에 m가 붙어있으면 미터 값을 센티미터로 변환하고 출력한다.
- 만약 지원하지 않는 길이 단위(feet)가 붙어 있을 경우, "지원하지 않는 단위입니다."를 출력하고 다시 입력받도록 한다.
+/* 로직 :
+ 1. 프로그램 구현부에서 유저 인풋값을 받음
+ 2. 인풋값이 프린트함수를 호출 : 프린트함수는 연산함수를 인수로 받음
+ 3. 변환함수가 아래의 함수를 호출함
+   함수1 : 유저인풋 받아서 배열로 나누기
+   함수2 : 갖고있는 단위인지 아닌지 판별하여 불값으로 리턴 (있으면 트루, 없으면 폴스)
+      - 갖고있는 단위가 없어서 false라면 "지원하지 않는 단위입니다." 출력하고 프로그램 다시 돌림
+   함수3 : 배열의 길이가 2인지 1인지 판별하여 배열을 튜플 스트링으로 리턴해줌
+      - 배열의 길이가 1이라면 현재갖고있는 단위를 확인하고 숫자와 단위로 리턴
+      - 배열의 길이가 2라면 현재단위와 목표단위와 숫자로 리턴
+   함수4 : 함수3번이 반환한 값을 인수로 받아 연산하는 함수
+      - 받은 인수에 따라 현재단위를 목표단위로 바꿔줄것인지, 현재단위만 cm-m을 상호 변환해줄것인지 선택하여 연산된 값을 리턴
+ 4. 3번의 변환이 끝난값을 2번의 프린트함수가 인수로 받아 결과를 출력
  */
 
 import Foundation
 
-// 함수0 : 사용자로부터 값을 입력받기
-func getUserInput () -> String {
-    let userInput = readLine() ?? ""
-    return userInput
+// 0. lengunit의 구조체 선언
+struct LengthUnit {
+    enum UnitNum: Float {
+        case cm = 1, m = 100, inch = 2.54
+    }
+    enum Unittype: String {
+        case cm, m, inch
+    }
+    // unit체크를 위해 배열에 넣음
+    let lengthUnitType = [Unittype.cm.rawValue, Unittype.m.rawValue, Unittype.inch.rawValue]
 }
 
-// 함수1 : meter 단위제거 : trimmingCharacters사용
-func eraseMeterUnit (_ input: String) -> Int{
-    var num: Int = 0
-    num += Int(input.trimmingCharacters(in: ["m"]))!
-    return num
+// 0-1. unittype속성값에 접근하기 위해 LengthUnit의 인스턴스 생성
+var lengUnit = LengthUnit()
+
+// 1. 입력받은 값을 배열로 넘기기   ["123cm", "inch"]
+func divideStrToArr (_ input: String) -> [String] {
+    var initialArray: [String] = []
+    let userInput = input
+    initialArray = userInput.components(separatedBy: " ")
+    return initialArray
 }
 
-// 함수2 : meter를 centi로 변환
-func convertMeterToCenti (_ input: Int) -> String {
-    var result: String = ""
-    result += String(input*100) + "cm"
+// 2. 지원되는 단위인지 체크하기
+func unitCheck (_ input: [String]) -> Bool {
+    let array = input
+    var hasUnit: Bool = false
+    for i in lengUnit.lengthUnitType {
+        if array[0].hasSuffix(i) {
+            hasUnit = true
+            break
+        } else {
+            hasUnit = false
+        }
+    }
+    return hasUnit
+}
+
+// 3. 첫단위 쪼개기
+func findUnit (_ strArray: [String]) -> String {
+    var from: String = ""
+    for i in lengUnit.lengthUnitType {
+        if strArray[0].hasSuffix(i) {
+            from += i
+            return from
+        }
+    }
+    return from
+}
+
+// 4. 배열을 스트링으로 나누기 <배열 길이에 따라 분기>
+func devideArraytoString (_ input: [String]) -> (String, String, String) {
+    var (from, num, to) = ("", "", "")
+    if input.count == 2 {
+        from = findUnit(input)
+        let frontString = input[0]
+        num += frontString.prefix(frontString.count - from.count)
+        to = input[1]
+        
+    } else if input.count == 1 {
+        from = findUnit(input)
+        let frontString = input[0]
+        num += frontString.prefix(frontString.count - from.count)
+        if from == LengthUnit.Unittype.cm.rawValue {
+            to = LengthUnit.Unittype.m.rawValue
+        } else if from == LengthUnit.Unittype.m.rawValue {
+            to = LengthUnit.Unittype.cm.rawValue
+        } else {
+            print ("지원하지 목표단위 단위입니다") }
+    }
+    return (from, to, num)
+}
+
+// 5. 모든 함수를 호출하여 값을 반환하는 함수
+func excuteConverting (_ userInput: String?) -> String {
+    guard let userInput = userInput else { return "" }
+    var hasUnit: Bool = false
+    var result: String = "#변환된 값 : "
+    let divide = divideStrToArr(userInput)
+    hasUnit = unitCheck(divide)
+    if hasUnit == true {
+        let (from, to, num) = devideArraytoString(divide)
+        switch (from, to) {
+        case ("cm", "inch") :
+            result += "\((String(convertCentiToInch(Float(num) ?? 0)) + to))"
+        case ("cm", "m") :
+            result += "\((String(convertCentiToM(Float(num) ?? 0)) + to))"
+        case ("m", "inch") :
+            result += "\((String(convertCentiToInch(convertMeterToCenti(Float(num) ?? 0))) + to))"
+        case ("m", "cm") :
+            result += "\((String(convertMeterToCenti(Float(num) ?? 0)) + to))"
+        case ("inch", "m") :
+            result += "\((String(convertCentiToM(convertInchToCenti(Float(num) ?? 0))) + to))"
+        case ("inch", "cm") :
+            result += "\((String(convertInchToCenti(Float(num) ?? 0)) + to))"
+        default :
+            result += ("지원하지 않는 단위입니다.")
+        }
+    } else {
+        result += ("지원하지 않는 단위입니다.")
+    }
+    return result
+}
+// 6. 각 연산 함수
+
+// : meter를 centi로 변환
+func convertMeterToCenti (_ input: Float) -> Float {
+    var result: Float = 0.0
+    result += input*LengthUnit.UnitNum.m.rawValue
     return result
 }
 
-// 함수3 : centi 단위제거 : prefix upto와 index사용
-func eraseCentiUnit (_ input: String) -> Double{
-    var num: Double = 0.0
-    num += Double(input.prefix(upTo: input.index(of: "c")!))!
-    return num
-}
-
-// 함수4 : centi를 meter로 변환
-func convertCentiToM (_ input: Double) -> String {
-    var result: String = ""
-    result += String(input/100) + "m"
+// : centi를 meter로 변환
+func convertCentiToM (_ input: Float) -> Float {
+    var result: Float = 0.0
+    result += input/LengthUnit.UnitNum.m.rawValue
     return result
 }
 
-// 함수 5: 출력
+// : centi를 inch로 변환
+func convertCentiToInch (_ input: Float) -> Float {
+    var result: Float = 0.0
+    result += input/LengthUnit.UnitNum.inch.rawValue
+    return result
+}
+
+// : inch를 centi로 변환
+func convertInchToCenti (_ input: Float) -> Float {
+    var result: Float = 0.0
+    result += input*LengthUnit.UnitNum.inch.rawValue
+    return result
+}
+
+// 7. 출력
 func printResult (_ result: String) {
     print(result)
 }
 
-// 프로그램 루프
+// 8. 프로그램 구현부
 unitConvertLoop : while true {
     print ("변환하고 싶은 단위를 입력해주세요.")
-    let input = getUserInput()
-    if input == "q" || input == "quit" { break }
-    
-    var result: String = ": "
-    if input.hasSuffix("cm") {
-        result += convertCentiToM(eraseCentiUnit(input))
-    } else if input.hasSuffix("m") {
-        result += convertMeterToCenti(eraseMeterUnit(input))
-    } else {
-        print ("지원하지 않는 단위입니다.")
-        continue unitConvertLoop
-    }
-    printResult(result)
+    let userInput = readLine()
+    if userInput == "q" || userInput == "quit" { break }
+    printResult(excuteConverting(userInput))
+    print("------------------\n")
 }
-
